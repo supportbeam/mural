a5395b98261d342fc4d85940a874bf81575a9e64
Duchess <33904740+Rmnlly@users.noreply.github.com>
2017-12-14 14:18:17 -0500

    First update on backend

---
+++ index.js
@@ -0,0 +1,63 @@
+let app = require('express')();
+let http = require('http').Server(app);
+let io = require('socket.io')(http);
+
+app.get('/', function(req, res){
+  res.sendFile(__dirname + '/index.html');
+});
+
+let numUsers = 0;
+
+io.on('connection', function(socket){
+  socket.on('color', (name, fn) => {
+    fn(name);
+    io.emit('color', name); //this will send the data to help us display the typed name for everyone
+  });
+});
+
+io.on('connection', function(socket){
+
+  let addedUser = false;
+
+  //when the client emits the "add user" event, this listens and executes
+  socket.on('add user', function(username){
+    if (addedUser) return;
+
+    //We'll store their usename in the socket session for this client
+    socket.username = username;
+    ++numUsers;
+    addedUser = true;
+    socket.emit('login', {
+      numUsers: numUsers
+    });
+
+    //Echo to everyone that someone connected
+    socket.broadcast.emit('user joined', {
+      username: socket.username,
+      numUsers: numUsers
+    });
+  });
+
+  socket.on('disconnect', function(){
+    if(addedUser) {
+      --numUsers;
+
+      socket.broadcast.emit('user left', {
+        username: socket.username,
+        numUsers: numUsers
+      });
+    }
+  });
+
+  socket.on('chat message', function(msg){
+    io.emit('chat message', msg);
+    socket.broadcast.emit('chat message', msg);
+  });
+
+
+
+});
+
+http.listen(3000, '0.0.0.0', function(){
+  console.log('listening on *:3000');
+});
